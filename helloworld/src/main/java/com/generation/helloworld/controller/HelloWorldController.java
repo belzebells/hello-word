6e2f49e49954fc8312b4c23db3bcddc3ae0bb985
izabela bonzanini <100371304+izabelabonzanini@users.noreply.github.com>
2023-08-30 10:34:29 -0300

    Desenvolvendo o Backend com Spring
    1. Crie um repositório chamado hello_world no Github
    2. Desenvolva a aplicação abaixo
    3. Ao final desta atividade, inicialize a pasta do projeto como um repositório git e faça a conexão com o repositório remoto
    4. Envie o projeto para o repositório remoto
    5. Na entrega do Projeto na Plataforma, efetue a validação da Classe Controladora, utilizando a camada controller.
       
    ATIVIDADE 1
    1. Crie um projeto Spring com 3 endpoints: 
    /hello-word: O primeiro endpoint deverá exibir a mensagem: Hello World!
    
    / BSM's da Generation: O segundo endpoint deverá exibir a lista de BSM’s da Generation Brasil.
    
    /objetivos de aprendizagem desta semana: O terceiro endpoint deverá exibir uma lista contendo os seus objetivos de aprendizagem desta semana.

--- helloworld/src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -5,44 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/hello-world")
+@RequestMapping("/hello-world")
 
 public class HelloWorldController {
-
+	
 	@GetMapping
 	public String helloWorld() {
 		return "Hello World!";
-	}
-	
-	@GetMapping
-	public String listaBsms() {
-		return "BSM's da Generation!"
-				+ "\nComunicação;"
-				+ "\nProatividade;"
-				+ "\nOrientação ao detalhe;"
-				+ "\nTrabalho em equipe;"
-				+ "\nOrientação ao futuro;"
-				+ "\nResponsabilidade social;"
-				+ "\nMentalidade de crescimento;"
-				+ "\nPersistência";
-	}
-	
-	@GetMapping
-	public String objetivosAprendizagem() {
-		return "objetivos de aprendizagem desta semana:"
-				+ "fazer todos os exercícios no dia certo;"
-				+ "rever a matéria passada no dia;"
-				+ "entregar todas as atividades;"
-				+ "tirar dúvidas com as pessoas instrutoras e da turma;"
-				+ "não desistir;";		
-						
-	} 	
 		
-		
-	
+	}
 
-	
-				
-	}
-	
-	
+}
